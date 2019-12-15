2c7a1751ec2293ed4e9c72de0412d58dd9e94f37
magiQAQ <39616939+magiQAQ@users.noreply.github.com>
2019-12-15 21:20:35 +0800

    Android动画的Demo

--- animation/src/main/java/com/magi/mydemos/FrameAnimationActivity.java
+++ animation/src/main/java/com/magi/mydemos/FrameAnimationActivity.java
@@ -2,13 +2,32 @@ package com.magi.mydemos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 
 public class FrameAnimationActivity extends AppCompatActivity {
+
+    private AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame_animation);
+
+        View view= findViewById(R.id.view);
+        animationDrawable = (AnimationDrawable) view.getBackground();
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btnStart:
+                animationDrawable.start();
+                break;
+            case R.id.btnStop:
+                animationDrawable.stop();
+                break;
+        }
     }
 }
