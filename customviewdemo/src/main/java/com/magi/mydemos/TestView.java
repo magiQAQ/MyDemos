29ab9ed2b5da96ea0dc4899e2f4b0de1b09835f1
magiQAQ <39616939+magiQAQ@users.noreply.github.com>
2019-12-19 23:13:03 +0800

    自定义控件的Demo

---
+++ customviewdemo/src/main/java/com/magi/mydemos/TestView.java
@@ -0,0 +1,25 @@
+package com.magi.mydemos;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class TestView extends View {
+    public TestView(Context context) {
+        super(context);
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
